9d53a2d2ea621a57f95133ab0214c9000bb6877c
jpclark6 <jpclark6@gmail.com>
2018-11-02 17:42:27 -0600

    Adds runner files

---
+++ lib/encrypt.rb
@@ -0,0 +1,9 @@
+require './lib/enigma'
+require './file_io'
+
+
+# ARGV[0] == "red.txt"
+# ARGV[1] == "blue.txt"
+#
+# $ ruby ./lib/encrypt.rb message.txt encrypted.txt
+# Created 'encrypted.txt' with the key 82648 and date 240818
