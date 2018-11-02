9759fa9096e709a6becba8a42121569baf8665cf
jpclark6 <jpclark6@gmail.com>
2018-11-02 11:48:40 -0600

    Adds some to encrypt, needs a new test/method now

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,9 +1,13 @@
+require './lib/rotation_finder'
+
 class Enigma
   def alpha
     ("a".."z").to_a << " "
   end
 
   def encrypt(string, numbers, date = find_date)
+    rotations = RotationFinder(numbers, date)
+    encrypted = cycle(string, rotations)
   end
 
   def find_date
