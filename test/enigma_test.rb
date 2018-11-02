7ab1809174250ce83de67eb10dd326f9e280268e
jpclark6 <jpclark6@gmail.com>
2018-11-02 11:24:34 -0600

    Adds alphabet test to enigma

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,4 +8,11 @@ class EnigmaTest < Minitest::Test
     e = Enigma.new
     assert_instance_of Enigma, e
   end
+
+  def test_it_has_alpha_and_space
+    e = Enigma.new
+    assert_instance_of Array, e.alpha
+    assert_equal "a", e.alpha.first
+    assert_equal " ", e.alpha[26]
+  end
 end
