47db33d1fdf5bc7415c3fbb3840fdd1bdd2a0783
jpclark6 <jpclark6@gmail.com>
2018-11-02 17:51:24 -0600

    First iteration of encrypt runner

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,16 +1,18 @@
 require './lib/enigma'
-require './file_io'
+require './lib/file_io'
 
 file_to_convert = ARGV[0]
 file_path = ARGV[1]
 
 file_io = FileIO.new
-
-string_to_convert = file_io.read(file_to_convert)
-
 enigma = Enigma.new
 
-converted_string = enigma.encrypt(string_to_convert)
+string_to_convert = file_io.read(file_to_convert)
+encryption_data = enigma.encrypt(string_to_convert)
+file_io.write(encryption_data[:encryption], file_path)
+
+puts "Created '#{file_path}' with the key #{:key} and date #{:date}"
+
 
 # ARGV[0] == "red.txt"
 # ARGV[1] == "blue.txt"
