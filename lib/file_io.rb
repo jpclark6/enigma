453b499e52e3b00b1cdc6c3f6db146f78ae3e885
jpclark6 <jpclark6@gmail.com>
2018-11-02 17:34:17 -0600

    Adds read/write functionality

--- lib/file_io.rb
+++ lib/file_io.rb
@@ -1,3 +1,13 @@
 class FileIO
-  
+  def write(string, file)
+    new_file = File.open(file, "w")
+    new_file.write(string)
+    new_file.close
+    "Success"
+  end
+
+  def read(file)
+    file = File.open(file, "r")
+    file.read
+  end
 end
