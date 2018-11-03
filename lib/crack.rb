715a9676173f2fb592bc7d4db4bf8129fc7cb823
jpclark6 <jpclark6@gmail.com>
2018-11-02 19:15:02 -0600

    Cracker command line possibly done

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,17 +1,13 @@
 require './lib/enigma_cracker'
 require './lib/file_io'
 
-file_to_convert = ARGV[0]
-file_path = ARGV[1]
+file_to_crack = ARGV[0]
+cracked_file = ARGV[1]
 date = ARGV[2]
 
 file_io = FileIO.new
 enigma_cracker = EnigmaCracker.new
 
-string_to_convert = file_io.read(file_to_convert).chomp
-encryption_data = enigma.encrypt(string_to_convert)
-file_io.write(encryption_data[:encryption], file_path)
-
-
-$ ruby ./lib/crack.rb encrypted.txt cracked.txt 240818
-Created 'cracked.txt' with the cracked key 82648 and date 240818
+string_to_crack = file_io.read(file_to_crack).chomp
+encryption_data = enigma_cracker.crack(string_to_crack, date)
+file_io.write(encryption_data[:decryption], cracked_file)
