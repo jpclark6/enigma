7189e50293e4d8634a5cc908a7e6d33649e4aacf
jpclark6 <jpclark6@gmail.com>
2018-11-02 18:09:12 -0600

    Finishes functionality for decrypt runner. Needs refactoring

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -12,3 +12,5 @@ enigma = Enigma.new
 string_to_decrypt = file_io.read(file_to_decrypt).chomp
 decryption_data = enigma.decrypt(string_to_decrypt, key, date)
 file_io.write(decryption_data[:decryption], file_path_for_text)
+
+puts "Created '#{file_path_for_text}' with the key #{decryption_data[:key]} and date #{decryption_data[:date]}"
