e40703c6e467229770b53bfc0b3679282eafc1f1
jpclark6 <jpclark6@gmail.com>
2018-11-02 18:56:51 -0600

    Adds test for formatter

--- test/formatter_test.rb
+++ test/formatter_test.rb
@@ -4,5 +4,16 @@ require 'minitest/pride'
 require './lib/formatter'
 
 class FormatterTest < Minitest::Test
+  def test_it_can_format_text_to_correct_output
+    expected =  {encryption: "keder ohulw",
+                  key: "02715",
+                  date: "040895"
+                }
+    encrypt_or_decrypt = :encryption
+    encrypted = "keder ohulw"
+    numbers = "02715"
+    date = "040895"
+    assert_equal expected, Formatter.format_return(encrypted, numbers, date, encrypt_or_decrypt)
+  end
 
 end
