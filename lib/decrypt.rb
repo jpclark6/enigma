802c0c4a5cefe0172f1bfc3d9ea5b7806cb1ecf9
jpclark6 <jpclark6@gmail.com>
2018-11-02 18:05:09 -0600

    Updates to possible decrypt runner functionality

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -6,15 +6,9 @@ file_path_for_text = ARGV[1]
 key = ARGV[2]
 date = ARGV[3]
 
-#
-# file_io = FileIO.new
-# enigma = Enigma.new
-#
-# string_to_convert = file_io.read(file_to_convert).chomp
-# encryption_data = enigma.encrypt(string_to_convert)
-# file_io.write(encryption_data[:encryption], file_path)
-#
-# puts "Created '#{file_path}' with the key #{encryption_data[:key]} and date #{encryption_data[:date]}"
-#
-# $ ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818
-# Created 'decrypted.txt' with the key 82648 and date 240818
+file_io = FileIO.new
+enigma = Enigma.new
+
+string_to_decrypt = file_io.read(file_to_decrypt).chomp
+decryption_data = enigma.decrypt(string_to_decrypt, key, date)
+file_io.write(decryption_data[:decryption], file_path_for_text)
