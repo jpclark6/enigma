ede58abeee6dd4e64eb6e5077946cbe96821fc38
jpclark6 <jpclark6@gmail.com>
2018-11-02 18:32:44 -0600

    Cracker passes existance test

--- test/enigma_cracker_test.rb
+++ test/enigma_cracker_test.rb
@@ -1,11 +1,12 @@
 require './test/test_helper'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/enigma_cracker'
 
 class EnigmaCrackerTest < Minitest::Test
   def test_it_exists
     ec = EnigmaCracker.new
     assert_instance_of EnigmaCracker, ec
   end
-  
+
 end
