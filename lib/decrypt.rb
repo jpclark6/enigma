069ea6ed8a0f722f5cd56658e4ae4a1d880aba74
jpclark6 <jpclark6@gmail.com>
2018-11-02 18:00:03 -0600

    Adds comments to decrypt to start

---
+++ lib/decrypt.rb
@@ -0,0 +1,17 @@
+require './lib/enigma'
+require './lib/file_io'
+
+# file_to_convert = ARGV[0]
+# file_path = ARGV[1]
+#
+# file_io = FileIO.new
+# enigma = Enigma.new
+#
+# string_to_convert = file_io.read(file_to_convert).chomp
+# encryption_data = enigma.encrypt(string_to_convert)
+# file_io.write(encryption_data[:encryption], file_path)
+#
+# puts "Created '#{file_path}' with the key #{encryption_data[:key]} and date #{encryption_data[:date]}"
+#
+# $ ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818
+# Created 'decrypted.txt' with the key 82648 and date 240818
