e6cfac8a11e420be50a51db6eb6f45c684b974d6
jpclark6 <jpclark6@gmail.com>
2018-11-02 19:06:24 -0600

    Removes note in cracker to tell user where it is at

--- lib/enigma_cracker.rb
+++ lib/enigma_cracker.rb
@@ -4,9 +4,6 @@ class EnigmaCracker
   def crack(encryption, date)
     e = Enigma.new
     all_key_combos.each_with_index do |key, i|
-      # if i%1000 == 0
-      #   puts "Trying! Iteration #{i}, key: #{key.join}"
-      # end
       data = e.decrypt(encryption, key.join, date)
       return data if data[:decryption][-4..-1] == " end"
     end
